fa66110c7992cc35273b48a9b68318bcc196a686
Eric Long <Ericsmainemail@gmail.com>
2026-03-10 10:35:26 -0700

    worked through interval dp transitions for palindromic subsequence

---
+++ dynamic-programming/longestPalindromicSubsequence.ts
@@ -0,0 +1,32 @@
+/**
+ * Longest Palindromic Subsequence
+ *
+ * Pattern: Interval DP
+ * Time: O(n^2)
+ * Space: O(n^2)
+ *
+ * Notes:
+ * dp[i][j] represents longest palindromic subsequence
+ * within substring s[i..j].
+ */
+
+export function longestPalindromicSubsequence(s: string): number {
+  const n = s.length;
+  const dp: number[][] = Array.from({ length: n }, () =>
+    new Array(n).fill(0)
+  );
+
+  for (let i = n - 1; i >= 0; i--) {
+    dp[i][i] = 1;
+
+    for (let j = i + 1; j < n; j++) {
+      if (s[i] === s[j]) {
+        dp[i][j] = dp[i + 1][j - 1] + 2;
+      } else {
+        dp[i][j] = Math.max(dp[i + 1][j], dp[i][j - 1]);
+      }
+    }
+  }
+
+  return dp[0][n - 1];
+}
