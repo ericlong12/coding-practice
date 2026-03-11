3710a8b20d2e7a744a5f89da7593099769ec7ad7
Eric Long <Ericsmainemail@gmail.com>
2026-03-11 08:59:17 -0700

    added auxiliary stack to support constant time minimum retrieval

---
+++ design/minStack.ts
@@ -0,0 +1,47 @@
+/**
+ * Min Stack
+ *
+ * Pattern: Augmented Stack
+ * Time: O(1)
+ * Space: O(n)
+ *
+ * Notes:
+ * Maintain a second stack tracking minimum values.
+ */
+
+export class MinStack {
+  private stack: number[];
+  private minStack: number[];
+
+  constructor() {
+    this.stack = [];
+    this.minStack = [];
+  }
+
+  push(val: number): void {
+    this.stack.push(val);
+
+    if (
+      this.minStack.length === 0 ||
+      val <= this.minStack[this.minStack.length - 1]
+    ) {
+      this.minStack.push(val);
+    }
+  }
+
+  pop(): void {
+    const removed = this.stack.pop();
+
+    if (removed === this.minStack[this.minStack.length - 1]) {
+      this.minStack.pop();
+    }
+  }
+
+  top(): number {
+    return this.stack[this.stack.length - 1];
+  }
+
+  getMin(): number {
+    return this.minStack[this.minStack.length - 1];
+  }
+}
