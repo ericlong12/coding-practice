7384e520af445c56fc064bef68d52831cc058ae9
Eric Long <Ericsmainemail@gmail.com>
2026-03-15 09:42:15 -0700

    implemented monotonic deque to maintain sliding window maximum

---
+++ arrays/slidingWindowMaximum.ts
@@ -0,0 +1,34 @@
+/**
+ * Sliding Window Maximum
+ *
+ * Pattern: Monotonic Deque
+ * Time: O(n)
+ * Space: O(k)
+ *
+ * Notes:
+ * Maintain a decreasing deque of indices
+ * representing candidate maximums.
+ */
+
+export function slidingWindowMaximum(nums: number[], k: number): number[] {
+  const deque: number[] = [];
+  const result: number[] = [];
+
+  for (let i = 0; i < nums.length; i++) {
+    while (deque.length && deque[0] <= i - k) {
+      deque.shift();
+    }
+
+    while (deque.length && nums[deque[deque.length - 1]] < nums[i]) {
+      deque.pop();
+    }
+
+    deque.push(i);
+
+    if (i >= k - 1) {
+      result.push(nums[deque[0]]);
+    }
+  }
+
+  return result;
+}
