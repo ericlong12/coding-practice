ff3bdbae17fda3ee5a03518119aec7d7462aba33
Eric Long <Ericsmainemail@gmail.com>
2026-03-23 09:35:32 -0700

    refined solved inventory to skip non-problem typescript files

--- scripts/solved-inventory.sh
+++ scripts/solved-inventory.sh
@@ -6,10 +6,14 @@ ROOT_DIR="${1:-.}"
 find "$ROOT_DIR" \
   -type f \
   -name "*.ts" \
+  ! -name "*.test.ts" \
+  ! -name "*.spec.ts" \
+  ! -name "*.d.ts" \
   ! -path "*/node_modules/*" \
   ! -path "*/dist/*" \
   ! -path "*/build/*" \
   ! -path "*/coverage/*" \
   ! -path "*/scripts/*" \
+  ! -path "*/utils/*" \
   | sed "s#^\./##" \
   | sort
